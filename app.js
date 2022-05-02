a9b26c0ac8d941a122e3a6232f6528b36829a755
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-02 11:41:57 -0400

    Testing CORS fix

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
 }));
 
