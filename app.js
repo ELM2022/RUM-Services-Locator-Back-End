f054e03c4dc21698ea37db35dcb3750cf0926e5d
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-02 14:57:34 -0400

    Testing cookie fix 2

--- app.js
+++ app.js
@@ -42,9 +42,11 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
-    // cookie: {
-    //     maxAge: 1000 * 60 * 60 * 24     // one day
-    // }
+    cookie: {
+        httpOnly: false,
+        secure: false,
+        maxAge: 1000 * 60 * 60 * 24     // one day
+    }
 }));
 
 // SETTING UP AUTHENTICATION
