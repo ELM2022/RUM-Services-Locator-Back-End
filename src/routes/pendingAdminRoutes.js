4becb4ac25e4e58cae52b46419be887e6e010281
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-08 16:54:30 -0400

    Pending admin by email route fix (test)

--- src/routes/pendingAdminRoutes.js
+++ src/routes/pendingAdminRoutes.js
@@ -6,9 +6,9 @@ const { validateRoute } = require('../middlewares/validateMiddleware');
 const { pendingAdminPostRules } = require('../middlewares/validationRules/pendingAdminValidation');
 
 router.get("/admin/pending/unresolved", pendingAdminController.getUnresolvedPendingAdmins);
+router.get("admin/pending/email", pendingAdminController.getPendingAdminByEmail);
 router.get("/admin/pending/all", pendingAdminController.getAllPendingAdmins);
 router.get("/admin/pending/:paid", pendingAdminController.getPendingAdminById);
-router.get("admin/pending/email", pendingAdminController.getPendingAdminByEmail);
 router.post("/admin/pending", pendingAdminPostRules(), validateRoute, pendingAdminController.addPendingAdmin);
 router.put("/admin/pending/:paid/delete", pendingAdminController.deletePendingAdmin);
 
