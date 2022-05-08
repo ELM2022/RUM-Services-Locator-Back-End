a6c75368281ab1debc35ffca59da8701e10ffc4f
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-08 18:34:01 -0400

    Testing get admin updates with editor admin name

--- src/controllers/adminUpdatesController.js
+++ src/controllers/adminUpdatesController.js
@@ -23,7 +23,9 @@ const addAdminUpdate = async(req, res) => {
 const getAllAdminUpdates = async(req, res) => {
     try {
         db.query(
-            `SELECT editor_admin_id, updated_admin_id, admin_email AS updated_admin_email, admin_name AS updated_admin_name, admin_last_name AS updated_admin_last_name, update_datetime, update_justification 
+            `SELECT editor_admin_id, 
+            (SELECT admin_name FROM Administrator WHERE admin_id = editor_admin_id) AS editor_admin_name,
+            updated_admin_id, admin_email AS updated_admin_email, admin_name AS updated_admin_name, admin_last_name AS updated_admin_last_name, update_datetime, update_justification 
             FROM Admin_Record_Updates 
             INNER JOIN Administrator ON Admin_Record_Updates.updated_admin_id = Administrator.admin_id`,
             (error, results) => {
