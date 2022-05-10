30cea6a1ef731220a5586b465dd88fe924099503
Maria Vilanova <maria.vilanova@upr.edu>
2022-05-10 15:46:06 -0400

    Adding ignored chars to office description

--- src/middlewares/validationRules/officeValidation.js
+++ src/middlewares/validationRules/officeValidation.js
@@ -20,7 +20,7 @@ const officePostRules = () => {
 const officeUpdateRules = () => {
     return [
         body('office.office_name').notEmpty().isAlpha('es-ES', { ignore: ' .-/' }).withMessage("Office name must be a string with only letters."),
-        body('office.office_description').notEmpty().isAlphanumeric('es-ES', { ignore: ' ,.' }).withMessage("Office description must be a string with only letters or numbers."),
+        body('office.office_description').notEmpty().isAlphanumeric('es-ES', { ignore: ' ,.;' }).withMessage("Office description must be a string with only letters or numbers."),
         body('office.office_schedule').notEmpty(),
         body('office.office_latitude').isFloat().withMessage("Office latitude must be a float number."),
         body('office.office_longitude').isFloat().withMessage("Office longitude must be a float number."),
